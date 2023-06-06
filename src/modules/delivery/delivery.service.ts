931d62e59210882cc9c4b23d0ec4afc214a4fcad
hwapedro <dmitriybabinup@gmail.com>
2023-06-06 21:12:09 +0700

    ☄ add cron for update delivery status

--- src/modules/delivery/delivery.service.ts
+++ src/modules/delivery/delivery.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { Cron } from '@nestjs/schedule';
 import { Model } from 'mongoose';
 
 import { BaseService } from '@/utils/services';
 
-import { DeliveryDocument, Delivery } from './entities';
+import { DeliveryDocument, Delivery, DeliveryStatus } from './entities';
 
 export const OTP_EXPIRED_TIME = 10 * 60000;
 
@@ -12,5 +13,22 @@ export const OTP_EXPIRED_TIME = 10 * 60000;
 export class DeliveryService extends BaseService<DeliveryDocument, Delivery> {
   constructor(@InjectModel(Delivery.name) private OtpModel: Model<DeliveryDocument>) {
     super(OtpModel);
+  }
+
+  @Cron('0 0 */5 * * *')
+  async handleCron() {
+    const deliveries = await this.find({
+      $and: [
+        { status: { $not: DeliveryStatus.SUCCESS } },
+        { status: { $not: DeliveryStatus.CANCELED } }
+      ]
+    });
+
+    const randomDeliveries = deliveries.filter(() => Math.random() < 0.3);
+
+    await this.updateMany(
+      { _id: { $in: randomDeliveries.map((delivery) => delivery._id) } },
+      { $inc: { status: 1 } }
+    );
   }
 }
